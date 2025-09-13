1299660266e77f577c06661d9bae4266df8da76e
Quang Lam <laulua1223@outlook.com>
2025-09-13 18:00:05 +0700

    add: depth limited search algorithm

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,4 +1,5 @@
 from .bfs import breadth_first_search
 from .dfs import depth_first_search
 from .ucs import uniform_cost_search
-__all__ = ["breadth_first_search", "depth_first_search", "uniform_cost_search"]
+from .dls import depth_limited_search
+__all__ = ["breadth_first_search", "depth_first_search", "uniform_cost_search", "depth_limited_search"]
