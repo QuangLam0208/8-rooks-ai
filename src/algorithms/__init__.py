563deeafb6625d3cc1e2c5679bfe500b80d8d009
Quang Lam <laulua1223@outlook.com>
2025-09-13 16:32:31 +0700

    add: ucs algorithm

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,4 +1,4 @@
 from .bfs import bfs_rooks
 from .dfs import dfs_rooks
-
-__all__ = ["bfs_rooks", "dfs_rooks"]
+from .ucs import ucs_rooks_goal
+__all__ = ["bfs_rooks", "dfs_rooks", "ucs_rooks_goal"]
